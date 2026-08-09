3064cff4f1b3a50440688ee634cda31a1de2cfed
juancot19 <juancot.pe@gmail.com>
2026-08-09 11:22:51 +0200

    feat: add session middleware

--- api/src/app.js
+++ api/src/app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const session = require("./lib/session");
 
 const app = express();
 
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(session);
 
 app.get("/api/v0/status", (req, res) => {
   res.json({
