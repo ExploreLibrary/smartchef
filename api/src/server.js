a7f1b0d0f151fb1a132e85375a435884ef2facfa
juancot19 <juancot.pe@gmail.com>
2026-08-09 11:44:32 +0200

    feat: add database connection

--- api/src/server.js
+++ api/src/server.js
@@ -1,19 +1,10 @@
 require("dotenv").config();
 
-const mongoose = require("mongoose");
 const app = require("./app");
+require("./lib/db");
 
 const PORT = process.env.PORT || 3000;
 
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => {
-    console.log("Connected to MongoDB");
-
-    app.listen(PORT, () => {
-      console.log(`API running on http://localhost:${PORT}`);
-    });
-  })
-  .catch((error) => {
-    console.error("Error connecting to MongoDB:", error);
-  });
+app.listen(PORT, () => {
+  console.log(`API running on http://localhost:${PORT}`);
+});
