9b44ba90f508e688d8b2fe2ddb00ddfbc4265106
David Masegosa <davidmasegosa@gmail.com>
2026-08-18 19:52:32 +0200

    session

--- api/src/lib/session.js
+++ api/src/lib/session.js
@@ -1,16 +1,15 @@
 const session = require("express-session");
 const { MongoStore } = require("connect-mongo");
+const config = require("./config");
 
-const sessionMiddleware = session({
-  secret: process.env.SESSION_SECRET,
+module.exports = session({
+  secret: config.get("session.secret"),
   resave: false,
   saveUninitialized: false,
-  store: MongoStore.create({
-    mongoUrl: process.env.MONGODB_URI,
-  }),
+  store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI }),
   cookie: {
+    httpOnly: true,
     maxAge: 1000 * 60 * 60 * 24,
+    secure: config.get("session.secure"),
   },
 });
-
-module.exports = sessionMiddleware;
