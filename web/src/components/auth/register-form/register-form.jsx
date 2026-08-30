6d529bd571ecb65a59bd1d654cbc9cb22ec7fa3a
David Masegosa <davidmasegosa@gmail.com>
2026-08-30 12:22:10 +0200

    register - form - stgyles

--- web/src/components/auth/register-form/register-form.jsx
+++ web/src/components/auth/register-form/register-form.jsx
@@ -24,43 +24,60 @@ function RegisterForm() {
   }
 
   return (
-    <form onSubmit={handleSubmit(handleUserRegister)}>
+    <div className="register-form__container">
+    <form onSubmit={handleSubmit(handleUserRegister)} className="register-form">
 
-      {/* NAME */}
-      <div className="input-group mb-1">
-        <span className="input-group-text"><i className="fa fa-user fa-fw"></i></span>
-        <input type="text" {...register('name', { required: 'User name is required'})} className={`form-control ${errors.name ? 'is-invalid' : ''}`} placeholder="Name" />
-        {errors.name && (<div className="invalid-feedback">{errors.name.message}</div>)}
+      <div className="register-form__header">
+        <h4 className="register-form__title">Register</h4>
       </div>
 
-      {/* USERNAME */}
-      <div className="input-group mb-1">
-        <span className="input-group-text"><i className="fa fa-tag fa-fw"></i></span>
-        <input type="text" {...register('username', { required: 'User username is required' })} className={`form-control ${errors.username ? 'is-invalid' : ''}`} placeholder="username" />
-        {errors.username && (<div className="invalid-feedback">{errors.username.message}</div>)}
+      <div className="register-form__body">
 
+        <div className="register-form__field">
+          {/* NAME */}
+          <div className="input-group mb-1">
+            <span className="input-group-text"><i className="fa fa-user fa-fw"></i></span>
+            <input type="text" {...register('name', { required: 'User name is required'})} className={`form-control ${errors.name ? 'is-invalid' : ''}`} placeholder="Name" />
+            {errors.name && (<p className="register-form__name-error">{errors.name.message}</p>)}
+          </div>
+        </div>
+
+        <div className="register-form__field">
+          {/* USERNAME */}
+          <div className="input-group mb-1">
+            <span className="input-group-text"><i className="fa fa-tag fa-fw"></i></span>
+            <input type="text" {...register('username', { required: 'User username is required' })} className={`form-control ${errors.username ? 'is-invalid' : ''}`} placeholder="username" />
+            {errors.username && (<p className="register-form__username-error">{errors.username.message}</p>)}
+          </div>
+        </div>
+
+        <div className="register-form__field">
+          {/* EMAIL */}
+          <div className="input-group mb-1">
+            <span className="input-group-text"><i className="fa fa-envelope-o fa-fw"></i></span>
+            <input type="email" {...register('email', { required: 'User email is required' })} className={`form-control ${errors.email ? 'is-invalid' : ''}`} placeholder="user@example.org" />
+            {errors.email && (<p className="register-form__email-error">{errors.email.message}</p>)}
+          </div>
+        </div>
+
+        <div className="register-form__field">
+          {/* PASSWORD */}
+          <div className="input-group mb-2">
+            <span className="input-group-text"><i className="fa fa-lock fa-fw"></i></span>
+            <input type="password" {...register('password', { required: 'User password is required' })} className={`form-control ${errors.password ? 'is-invalid' : ''}`} placeholder="***********" />
+            {errors.password && (<p className="register-form__password-error">{errors.password.message}</p>)}
+          </div>
+        </div>
+
+        <div className="login-form__footer">
+          <div className="d-grid gap-2">
+            <button className="login-form__submit-button" type="submit" disabled={!isValid}>Register</button>
+          </div>
+        </div>
       </div>
 
-      {/* EMAIL */}
-      <div className="input-group mb-1">
-        <span className="input-group-text"><i className="fa fa-envelope-o fa-fw"></i></span>
-        <input type="email" {...register('email', { required: 'User email is required' })} className={`form-control ${errors.email ? 'is-invalid' : ''}`} placeholder="user@example.org" />
-        {errors.email && (<div className="invalid-feedback">{errors.email.message}</div>)}
-      </div>
-
-      {/* PASSWORD */}
-      <div className="input-group mb-2">
-        <span className="input-group-text"><i className="fa fa-lock fa-fw"></i></span>
-        <input type="password" {...register('password', { required: 'User password is required' })} className={`form-control ${errors.password ? 'is-invalid' : ''}`} placeholder="***********" />
-        {errors.password && (<div className="invalid-feedback">{errors.password.message}</div>)}
-      </div>
-
-      <div className="d-grid gap-2">
-        <button className="btn btn-primary" type="submit" disabled={!isValid}>Register</button>
-        <hr className="m-0"/>
-        <button className="btn btn-secondary">Login</button>
-      </div>
     </form>
+    </div>
   )
 }
 
--- web/src/pages/register-page.jsx
+++ web/src/pages/register-page.jsx
@@ -5,7 +5,6 @@ import { RegisterForm } from "../components/auth";
 function RegisterPage() {
     return (
         <PageLayout>
-            <h1 className="welcome">Register Page</h1>
             <RegisterForm />
         </PageLayout>
     );
