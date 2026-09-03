6630aa958eda057a87c78823ce9b3f099c7acfe9
juancot19 <juancot.pe@gmail.com>
2026-09-03 19:17:26 +0200

    feat: recipe search

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -3,3 +3,4 @@ export { default as HomePage } from './home-page';
 export { default as RegisterPage } from './register-page';
 export { default as RecipeDetailPage } from './recipe-detail-page';
 export { default as FavoritesPage } from './favorites-page';
+export { default as RecipeSearchPage } from './recipe-search-page';
