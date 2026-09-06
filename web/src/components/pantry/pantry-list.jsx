a1e675418db4c91db6ec1ad851e53dbe7e5c547a
David Masegosa <davidmasegosa@gmail.com>
2026-09-06 11:05:01 +0200

    my pantry - styles

--- web/src/components/pantry/pantry-list.jsx
+++ web/src/components/pantry/pantry-list.jsx
@@ -5,24 +5,29 @@ function PantryList({ pantryData, onItemCreated, onItemDeleted }) {
   const items = Array.isArray(pantryData) ? pantryData : [];
 
   return (
-    <div className="pantry-list-container">
-        <div className="pantry-list">
-            {items.length > 0 ? (
-            <ul>
-                {items.map((item) => (
-                <li key={item.id}>
-                  <PantryItem item={item} onItemDeleted={onItemDeleted} />
-                </li>
-                ))}
-            </ul>
-            ) : (
-            <p>Your pantry is empty.</p>
-            )}
-        </div>
+    <div className="my-pantry__container">
+      <div className="pantry-list-container">
+          <h2>My Pantry</h2>
+          <div className="pantry-list">
+              {items.length > 0 ? (
+              <ul>
+                  {items.map((item) => (
+                  <li key={item.id}>
+                    <PantryItem item={item} onItemDeleted={onItemDeleted} />
+                  </li>
+                  ))}
+              </ul>
+              ) : (
+                <>
+                <p>Your pantry is empty.</p>
+              </>
+              )}
+          </div>
+      </div>
 
-        <div className="pantry-form-container">
-          <PantryForm onItemCreated={onItemCreated} />
-        </div>
+      <div className="pantry-form-container">
+        <PantryForm onItemCreated={onItemCreated} />
+      </div>
     </div>
   );
 }
